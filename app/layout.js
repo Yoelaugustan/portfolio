a10e2a7364b9bdc5b2966a2d24c4297e7f9a2a14
Yoel Augustan <yoelagustan@gmail.com>
2025-09-22 11:02:30 +0700

    Debug Font 2

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,17 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
-const outfit = Outfit({
-  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+export const outfit = Outfit({
+  subsets: ["latin"], 
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-outfit"
 });
 
-const ovo = Ovo({
-  subsets: ["latin"], weight: ["400"]
+export const ovo = Ovo({
+  subsets: ["latin"], 
+  weight: ["400"],
+  variable: "--font-ovo"
 });
-
 
 export const metadata = {
   title: "Portfolio",
@@ -18,9 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${outfit.className} ${ovo.className} antialiased`}
-      >
+      <body className={`${outfit.variable} ${ovo.variable} antialiased`}>
         {children}
       </body>
     </html>
