8e6ee6d6ab0b4ea44a1e5c8bc2003cef25d7cc44
Yoel Augustan <yoelagustan@gmail.com>
2025-09-22 16:33:07 +0700

    Navbar and Header Complete

---
+++ app/components/Header.jsx
@@ -0,0 +1,27 @@
+import { assets } from '@/assets/assets'
+import { ovo } from '@/lib/fonts'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
+      <div>
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+      </div>
+      <h1 className={`${ovo.className} text-3xl sm:text-6xl lg:text-[66px]`}>Hi! I'm Yoel Augustan</h1>
+      <h3 className={`${ovo.className} text-xl md:text-2xl`}>Undergraduate at Bina Nusantara University</h3>
+      <p className={`${ovo.className} max-w-2xl mx-auto`}>Interested in DL, ML and Full Stack Development with AI Integration</p>
+      <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+        <a href='#contact' className='px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2'>
+            Contact me <Image src={assets.right_arrow_white} className='w-4'/>
+        </a>
+        <a href='/sample-resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
+            My CV <Image src={assets.download_icon} className='w-4'/>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,4 @@
-import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
-
-export const outfit = Outfit({
-  subsets: ["latin"], 
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-outfit"
-});
-
-export const ovo = Ovo({
-  subsets: ["latin"], 
-  weight: ["400"],
-  variable: "--font-ovo"
-});
 
 export const metadata = {
   title: "Portfolio",
@@ -21,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${outfit.variable} ${ovo.variable} antialiased`}>
+      <body className="antialiased leading-8 overflow-x-hidden">
         {children}
       </body>
     </html>
