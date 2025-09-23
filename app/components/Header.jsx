9830b23010f6f03611d35f564dd5364248538621
Yoel Augustan <yoelagustan@gmail.com>
2025-09-23 18:00:47 +0700

    About Page Complete

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,13 +11,13 @@ const Header = () => {
       </div>
       <h1 className={`${ovo.className} text-3xl sm:text-6xl lg:text-[66px]`}>Hi! I'm Yoel Augustan</h1>
       <h3 className={`${ovo.className} text-xl md:text-2xl`}>Undergraduate at Bina Nusantara University</h3>
-      <p className={`${ovo.className} max-w-2xl mx-auto`}>Interested in DL, ML and Full Stack Development with AI Integration</p>
+      <p className={`${ovo.className} max-w-2xl mx-auto`}>Interested in Deep Learning, Machine Learning and Full Stack Development with AI Integration</p>
       <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
         <a href='#contact' className='px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2'>
-            Contact me <Image src={assets.right_arrow_white} className='w-4'/>
+            Contact me <Image src={assets.right_arrow_white} alt="" className='w-4'/>
         </a>
         <a href='/sample-resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
-            My CV <Image src={assets.download_icon} className='w-4'/>
+            My CV <Image src={assets.download_icon} alt="" className='w-4'/>
         </a>
       </div>
     </div>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,11 +2,12 @@
 import { assets } from '@/assets/assets'
 import { ovo } from '@/lib/fonts'
 import Image from 'next/image'
-import React, {useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
 
 const Navbar = () => {
 
+  const [isScroll, setIsScroll] = useState(false)
   const sideMenuRef = useRef();
   const openMenu = ()=>{
     sideMenuRef.current.style.transform = 'translateX(-16rem)'
@@ -14,20 +15,36 @@ const Navbar = () => {
   const closeMenu = ()=>{
     sideMenuRef.current.style.transform = 'translateX(16rem)'
   }
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScroll(true)
+      } else {
+        setIsScroll(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
 
   return (
     <>
       <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
         <Image src={assets.header_bg_color} alt='' className='w-full'/>
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white/50 backdrop-blur-lg shadow-xs" : ""}`}>
         <a href='#home'>
           <Image src={assets.logo} alt="" className='w-28 cursor-pointer mr-14'/>
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white/50 shadow-sm'>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white/50 shadow-sm"}`}>
           <li><a className={`${ovo.className}`} href='#home'>Home</a></li>
-          <li><a className={`${ovo.className}`}href='#about'>About me</a></li>
+          <li><a className={`${ovo.className}`} href='#about'>About me</a></li>
           <li><a className={`${ovo.className}`} href='#services'>Services</a></li>
           <li><a className={`${ovo.className}`} href='#work'>My Work</a></li>
           <li><a className={`${ovo.className}`} href='#contact'>Contact me</a></li>
